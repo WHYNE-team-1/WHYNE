2264b0d876b01d58706291c178e3b39828c134d1
여리의갤럭시북\pakjo <kaito5650@naver.com>
2026-03-20 02:24:39 +0900

    소셜 로그인 버튼 생성 완료

--- src/storybook/button/index.tsx
+++ src/storybook/button/index.tsx
@@ -1,10 +1,12 @@
-import GoogleLoginBtn from "@/components/common/button/GoogleLoginBtn";
-import KakaoLoginBtn from "@/components/common/button/KakaoLoginBtn";
+import GoogleLoginBtn from "@/components/common/button/social-login/GoogleLoginBtn";
+import KakaoLoginBtn from "@/components/common/button/social-login/KakaoLoginBtn";
 export default function buttonPage() {
   return (
     <div>
       <GoogleLoginBtn />
       <KakaoLoginBtn />
+      <GoogleLoginBtn size="S" />
+      <KakaoLoginBtn size="S" />
     </div>
   );
 }
