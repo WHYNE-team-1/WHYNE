5142a0f9bbe2ca37ac0c4bad3da94baee56088df
여리의갤럭시북\pakjo <kaito5650@naver.com>
2026-03-21 03:50:51 +0900

    modal button, x button 추가

--- src/components/common/button/AddReviewBtn.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./AddReviewBtn.module.css";
-
-export default function AddReviewBtn() {
-  return (
-    <button className={styles.button}>
-      <span className={styles.font}>리뷰 남기기</span>
-    </button>
-  );
-}
--- src/storybook/button/index.tsx
+++ src/storybook/button/index.tsx
@@ -1,19 +1,27 @@
-import LoginBtn from "@/components/common/button/loginBtn";
+import LoginBtn from "@/components/common/button/LoginBtn";
 import GoogleLoginBtn from "@/components/common/button/social-login/GoogleLoginBtn";
 import KakaoLoginBtn from "@/components/common/button/social-login/KakaoLoginBtn";
-import JoinBtn from "@/components/common/button/joinBtn";
-import AddReviewBtn from "@/components/common/button/AddReviewBtn";
+import JoinBtn from "@/components/common/button/JoinBtn";
+import ModalBtn from "@/components/common/button/Modal/ModalBtn";
+import AddReviewBtn from "@/components/common/button/Modal/AddReviewBtn";
+import XBtn from "@/components/common/button/IconBtn/XBtn";
+import XBtn2 from "@/components/common/button/IconBtn/XBtn2";
 
 export default function buttonPage() {
   return (
     <div>
-      <GoogleLoginBtn />
-      <KakaoLoginBtn />
+      <GoogleLoginBtn size="L" />
+      <KakaoLoginBtn size="L" />
       <GoogleLoginBtn size="S" />
       <KakaoLoginBtn size="S" />
       <LoginBtn />
       <JoinBtn />
       <AddReviewBtn />
+      <ModalBtn variant="cancel">취소</ModalBtn>
+      <ModalBtn variant="danger">삭제하기</ModalBtn>
+      <ModalBtn variant="primary">변경하기</ModalBtn>
+      <XBtn />
+      <XBtn2 />
     </div>
   );
 }
