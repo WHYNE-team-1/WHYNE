657a0307399e394551e8d1b6d468ea749a18af42
Song <ziy1027@naver.com>
2026-03-21 20:01:49 +0900

    [feat]: value, onChange 추가

--- src/components/common/PriceRangeSlider/index.tsx
+++ src/components/common/PriceRangeSlider/index.tsx
@@ -1,25 +1,29 @@
 import styles from "./index.module.css";
 import { Range, getTrackBackground } from "react-range";
-import { useState } from "react";
 
 type Props = {
+  value: number[];
+  onChange: (values: number[]) => void;
   max?: number;
 };
 
-export default function PriceRangeSlider({ max = 1000000 }: Props) {
-  const [values, setValues] = useState([0, max]);
+export default function PriceRangeSlider({
+  value,
+  onChange,
+  max = 1000000,
+}: Props) {
   return (
     <div className={styles.rangeWarp}>
       <output id="output" className={styles.rangeTop}>
-        <span>₩ {values[0].toLocaleString()}</span>
-        <span>₩ {values[1].toLocaleString()}</span>
+        <span>₩ {value[0].toLocaleString()}</span>
+        <span>₩ {value[1].toLocaleString()}</span>
       </output>
       <Range
         step={1000} // 이동 단위
         min={0} // 최소값
         max={max} // 최대값
-        values={values} // 현재 값 (배열!)
-        onChange={(values) => setValues(values)} // 변경
+        values={value} // 현재 값 (배열!)
+        onChange={onChange} // 변경
         renderTrack={({ props, children }) => (
           <div
             {...props}
@@ -27,7 +31,7 @@ export default function PriceRangeSlider({ max = 1000000 }: Props) {
               height: "6px",
               width: "100%",
               background: getTrackBackground({
-                values,
+                values: value,
                 colors: [
                   "var(--color-gray-100)",
                   "var(--color-primary-red)",
@@ -41,7 +45,7 @@ export default function PriceRangeSlider({ max = 1000000 }: Props) {
             {children}
           </div>
         )}
-        renderThumb={({ props, isDragged }) => (
+        renderThumb={({ props }) => (
           <div
             {...props}
             key={props.key}
