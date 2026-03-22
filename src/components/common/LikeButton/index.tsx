ed2936c09883d56fed79c39e597d5887ab9bc871
여리의갤럭시북\pakjo <kaito5650@naver.com>
2026-03-23 07:09:56 +0900

    좋아요 미완성

---
+++ src/components/common/LikeButton/index.tsx
@@ -0,0 +1,7 @@
+import styles from "./index.module.css";
+import likeAcitiveIcon from "@/assets/icons/ic-like-active.svg";
+import likeInactiveIcon from "@/assets/icons/ic-like-inactive.svg";
+
+export default function LikeButton() {
+  return <button></button>;
+}
