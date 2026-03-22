a582230ba28835439ed3aa9c3dcff98c052a0d5d
Song <ziy1027@naver.com>
2026-03-22 21:08:36 +0900

    [feat]: env파일 작성

--- src/pages/WinesDetail/index.tsx
+++ src/pages/WinesDetail/index.tsx
@@ -1,8 +1,14 @@
+import styels from "./index.module.css";
+import ReviewRating from "@/components/WineReview";
+
 function WinesDetail() {
   return (
     <div>
       <div>와인 상세</div>
-      <div>와인 리뷰</div>
+      <div className={styels.WineReviewWrap}>
+        {/* <ReviewList /> */}
+        <ReviewRating />
+      </div>
     </div>
   );
 }
