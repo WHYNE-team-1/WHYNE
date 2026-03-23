16e489b8c2c1337b3d96fe1615c8e954ceb665c6
bitna0a <qlcsk0147@naver.com>
2026-03-24 01:55:09 +0900

    [feat]: 헤더 구현

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,12 +1,63 @@
 import styles from "./index.module.css";
+import { useNavigate, Link } from "react-router-dom";
+import { useAuthStore } from "@/store/useAuthStore";
+import logoIcon from "@/assets/icons/ic-logo-white.svg";
+import DefaultProfile from "@/assets/icons/ic-default-profile.svg";
+import LinkButton from "@/components/common/LinkButton";
+import Dropdown from "@/components/common/Dropdown";
+import type { DropdownOption } from "@/components/common/Dropdown";
 
-function Header() {
+export default function Header() {
+  const { isLoggedIn, user, setLogout } = useAuthStore();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    setLogout();
+    localStorage.removeItem("accessToken");
+    navigate("/");
+  };
+
+  const profileOptions: DropdownOption[] = [
+    {
+      label: "마이페이지",
+      onClick: () => navigate("/"), // 추후 수정 필요
+    },
+    {
+      label: "로그아웃",
+      onClick: handleLogout,
+    },
+  ];
+
   return (
     <header className={styles.header}>
-      <nav className={styles.headerWrap}>
-        <h1 className={styles.logo}>헤더헤더</h1>
-      </nav>
+      <div className={styles.container}>
+        <Link to="/">
+          <img src={logoIcon} alt="WHYNE 로고" className={styles.logo} />
+        </Link>
+
+        {isLoggedIn ? (
+          <Dropdown
+            trigger={
+              <img
+                src={user?.image || DefaultProfile}
+                alt="프로필 메뉴 열기"
+                className={styles.profileImage}
+              />
+            }
+            options={profileOptions}
+            offset={18}
+          />
+        ) : (
+          <div className={styles.authButtons}>
+            <LinkButton to="/signin" color="whiteLine" size="headerIcon">
+              로그인
+            </LinkButton>
+            <LinkButton to="/signup" color="red" size="headerIcon">
+              회원가입
+            </LinkButton>
+          </div>
+        )}
+      </div>
     </header>
   );
 }
-export default Header;
