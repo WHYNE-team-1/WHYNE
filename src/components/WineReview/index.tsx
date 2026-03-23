3bdf059eeb840e61b550b2f8d8c39d267a49cf5c
Song <ziy1027@naver.com>
2026-03-23 22:07:23 +0900

    [feat] review modal 작업중

--- src/components/WineReview/index.tsx
+++ src/components/WineReview/index.tsx
@@ -1,15 +1,27 @@
-import Button from "@/components/common/button";
+import Button from "@/components/common/Button";
 import StarRating from "@/components/common/StarRating";
 import styles from "./index.module.css";
-import Modal from "@/components/common/modal";
+import Modal from "@/components/common/Modal";
 import { useState } from "react";
+import Textarea from "@/components/common/Textarea";
+import ReviewAromaCheckbox from "@/components/common/ReviewAromaCheckbox";
+import WineTasteSlider from "../common/WineTasteSlider";
 
 type Review = any;
 
 export default function WineReview({ data }: { data: Review | null }) {
   if (!data) return <div>로딩중...</div>;
   const [isOpen, setIsOpen] = useState(false);
+  const [content, setContent] = useState("");
+  const [aromas, setAromas] = useState<string[]>([]);
 
+  const [rating, setRating] = useState(3);
+
+  const modalButton = (
+    <Button color="black" size="stretch" type="submit">
+      리뷰 남기기
+    </Button>
+  );
   return (
     <>
       {" "}
@@ -50,10 +62,48 @@ export default function WineReview({ data }: { data: Review | null }) {
       <Modal
         isOpen={isOpen}
         onClose={() => setIsOpen(false)}
-        // title="리뷰 등록"
-        // buttonText="리뷰 등록하기기"
+        title="리뷰 등록"
+        footer={modalButton}
       >
-        ㅋㅋㅋ
+        <div className={styles.reviewTop}>
+          <div className={styles.wineInfo}>
+            <div className={styles.imgWrap}>
+              <img src={data.image} alt={data.image} />
+            </div>
+            <div className={styles.NameAndRegion}>
+              <p className={styles.name}>{data.name}</p>
+              <p className={styles.region}>{data.region}</p>
+            </div>
+          </div>
+          <div className={styles.wineRating}>
+            <StarRating
+              mode="interactive"
+              size="modal"
+              value={rating}
+              onChange={(val) => {
+                setRating(val); //
+              }}
+            />
+          </div>
+          <div className={styles.wineReview}>
+            <Textarea
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              placeholder="후기를 작성해주세요"
+              maxLength={400}
+            />
+          </div>
+        </div>
+        <div className={styles.reviewBottom}>
+          <div className={styles.taste}>
+            <p className={styles.q}>와인 맛은 어땠나요?</p>
+            <WineTasteSlider />
+          </div>
+          <div className={styles.aroma}>
+            <p className={styles.q}>기억에 남는 향이 있나요?</p>
+            <ReviewAromaCheckbox value={aromas} onChange={setAromas} />
+          </div>
+        </div>
       </Modal>
     </>
   );
--- src/storybook/rating/index.tsx
+++ src/storybook/rating/index.tsx
@@ -1,44 +1,38 @@
-import React, { useState } from 'react';
-import StarRating from '@/components/common/StarRating';
-import StarRatingBadge from '@/components/common/StarRatingBadge';
+import React, { useState } from "react";
+import StarRating from "@/components/common/StarRating";
+import StarRatingBadge from "@/components/common/StarRatingBadge";
 
 export default function StarRatingTestPage() {
-
   const [rating, setRating] = useState(3);
 
   return (
-    <div style={{ padding: '40px' }}>
-  
-
-
+    <div style={{ padding: "40px" }}>
       <h3> 와인 상세페이지 </h3>
       <StarRating mode="displayOnly" size="detail" value={rating} count={128} />
 
-      <div style={{ height: '40px' }} />
+      <div style={{ height: "40px" }} />
 
       <h3> 중간 리뷰 목록 안 </h3>
       <StarRating mode="displayOnly" size="list" value={rating} count={42} />
 
-      <div style={{ height: '40px' }} />
+      <div style={{ height: "40px" }} />
 
       <h3> 유저 리뷰 뱃지 (StarRatingBadge) </h3>
       <StarRatingBadge rating={rating} />
 
-      <div style={{ height: '40px' }} />
+      <div style={{ height: "40px" }} />
       <h3> 리뷰 남기기 모달 </h3>
-      
+
       <StarRating
         mode="interactive"
         size="modal"
         value={rating}
         onChange={(val) => {
-          setRating(val); // 
+          setRating(val); //
         }}
       />
 
-      <p>
-        현재 선택된 값: {rating}
-      </p>
+      <p>현재 선택된 값: {rating}</p>
     </div>
   );
 }
