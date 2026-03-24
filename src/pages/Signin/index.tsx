b6fcab7228fcea415307b6afe3e1ead640a8a8a2
bitna0a <qlcsk0147@naver.com>
2026-03-24 17:56:13 +0900

    [feat]: AuthLayout 컴포넌트 구현

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,5 +1,15 @@
-function Signin() {
-  return <div>로그인</div>;
-}
+import AuthLayout from "@/components/layout/AuthLayout";
 
-export default Signin;
+export default function Signin() {
+  return (
+    <AuthLayout
+      bottomText="계정이 없으신가요?"
+      bottomLinkText="회원가입 하기"
+      bottomLinkTo="/signup"
+    >
+      <div style={{ padding: "50px", textAlign: "center", backgroundColor: "#ffebee" }}>
+        로그인 폼 들어갈 자리
+      </div>
+    </AuthLayout>
+  );
+}
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,5 +1,15 @@
-function Signup() {
-  return <div>회원가입</div>;
-}
+import AuthLayout from "@/components/layout/AuthLayout";
 
-export default Signup;
+export default function Signup() {
+  return (
+    <AuthLayout
+      bottomText="계정이 이미 있으신가요?"
+      bottomLinkText="로그인 하기"
+      bottomLinkTo="/signin"
+    >
+      <div style={{ padding: "50px", textAlign: "center", backgroundColor: "#ffebee" }}>
+        회원가입 폼 들어갈 자리
+      </div>
+    </AuthLayout>
+  );
+}
