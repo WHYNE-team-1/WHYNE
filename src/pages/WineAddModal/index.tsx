b13f75a60a3f59e5254e3557eff41a7fd58153af
여리의갤럭시북\pakjo <kaito5650@naver.com>
2026-03-25 13:07:38 +0900

    와인 등록 모달 폴더 추가

---
+++ src/pages/WineAddModal/index.tsx
@@ -0,0 +1,11 @@
+import Modal from "@/components/common/Modal";
+import styles from "./index.module.css";
+
+export default function WineAddModal(){
+
+  return(<>
+    <Modal isOpen={true}>
+
+    </Modal>
+  </>);
+}
