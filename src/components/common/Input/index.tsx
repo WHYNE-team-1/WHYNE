624e48c7c79e075c2f09b94aad14968725604c34
bitna0a <qlcsk0147@naver.com>
2026-03-26 00:18:05 +0900

    [feat]: 로그인 및 회원가입 유효성 검사 로직 구현

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -1,4 +1,4 @@
-import { useId } from 'react';
+import { useId, forwardRef } from 'react';
 import type { InputHTMLAttributes } from 'react';
 import styles from './index.module.css';
 import ErrorIcon from '@/assets/icons/ic-error.svg';
@@ -9,52 +9,55 @@ export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string;
 }
 
-export default function Input({
-  label,
-  status = 'default',
-  errorMessage,
-  id,
-  className,
-  ...props
-}: InputProps) {
-  const uniqueId = useId();
-  const inputId = id || uniqueId;
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    { label, status = 'default', errorMessage, id, className, ...props },
+    ref
+  ) => {
+    const uniqueId = useId();
+    const inputId = id || uniqueId;
 
-  const inputStatusClass =
-    status === 'error' || status === 'modalError' ? styles.inputError : '';
+    const inputStatusClass =
+      status === 'error' || status === 'modalError' ? styles.inputError : '';
 
-  return (
-    <div className={`${styles.wrapper} ${className || ''}`}>
-      {(label || status === 'modalError') && (
-        <div className={styles.labelWrapper}>
-          {label && (
-            <label htmlFor={inputId} className={styles.label}>
-              {label}
-            </label>
-          )}
-          {status === 'modalError' && errorMessage && (
-            <span className={styles.topErrorMessage}>{errorMessage}</span>
+    return (
+      <div className={`${styles.wrapper} ${className || ''}`}>
+        {(label || status === 'modalError') && (
+          <div className={styles.labelWrapper}>
+            {label && (
+              <label htmlFor={inputId} className={styles.label}>
+                {label}
+              </label>
+            )}
+            {status === 'modalError' && errorMessage && (
+              <span className={styles.topErrorMessage}>{errorMessage}</span>
+            )}
+          </div>
+        )}
+
+        <div className={styles.inputContainer}>
+          <input
+            ref={ref}
+            id={inputId}
+            className={`${styles.input} ${inputStatusClass}`}
+            {...props}
+          />
+
+          {(status === 'error' || status === 'modalError') && (
+            <img src={ErrorIcon} alt="" className={styles.errorIcon} />
           )}
         </div>
-      )}
 
-      <div className={styles.inputContainer}>
-        <input
-          id={inputId}
-          className={`${styles.input} ${inputStatusClass}`}
-          {...props}
-        />
-
-        {(status === 'error' || status === 'modalError') && (
-          <img src={ErrorIcon} alt="" className={styles.errorIcon} />
-        )}
+        <div className={styles.errorWrap}>
+          {status === 'error' && errorMessage && (
+            <span className={styles.bottomErrorMessage}>{errorMessage}</span>
+          )}
+        </div>
       </div>
+    );
+  }
+);
 
-      <div className={styles.errorWrap}>
-        {status === 'error' && errorMessage && (
-          <span className={styles.bottomErrorMessage}>{errorMessage}</span>
-        )}
-      </div>
-    </div>
-  );
-}
+Input.displayName = 'Input';
+
+export default Input;
