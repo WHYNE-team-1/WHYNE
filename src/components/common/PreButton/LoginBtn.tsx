aa8e81dc00c597e55f84a2ff38fcd8f8635abd38
여리의갤럭시북\pakjo <kaito5650@naver.com>
2026-03-25 21:34:51 +0900

    기존 PreButton 폴더 삭제

--- src/components/common/PreButton/LoginBtn.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './LoginBtn.module.css';
-
-export default function LoginBtn() {
-  return (
-    <button className={styles.button}>
-      <span className={styles.font}>로그인</span>
-    </button>
-  );
-}
