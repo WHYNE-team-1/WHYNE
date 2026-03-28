2541e2fad80b2f7fcbded13a8bcdc21a5f77d494
Song <ziy1027@naver.com>
2026-03-28 20:37:22 +0900

    [feat] 로딩 페이지 추가

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,16 @@
+import styles from './index.module.css';
+export default function Loading() {
+  return (
+    <main role="loading" className={styles.loadingWrap}>
+      <div
+        className={styles.loading}
+        aria-busy="true"
+        aria-label="Loading, please wait."
+        data-text="WYHNE"
+        role="progressbar"
+      >
+        <p className={styles.loadingSubTxt}>Please wait</p>
+      </div>
+    </main>
+  );
+}
